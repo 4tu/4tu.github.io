83f8382f33340471292a6d62fd9bc5ccd255c28d
4tu <rikuto.tsujimoto++4tu@gmail.com>
2020-06-07 06:49:02 +0900

    add: headerとfooterの追加

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header-area">
+        <div class="logo-area">
+            <a class="title" href="/">
+                <span class="red">T</span>heory of Fortune.
+            </a>
+        </div>
+        <nav class="nav-area">
+            <div id="navbutton">
+                <div class="button" v-for="link in links" :key="link.name">
+                    <a class="text" href="/">{{ link }}</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return{
+            links: ['About', 'Stack', 'Contact'],
+        }
+    }
+}
+</script>
