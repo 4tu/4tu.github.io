c067f9962e8120531f6041cf43400ae7c9f47118
4tu <rikuto.tsujimoto++4tu@gmail.com>
2020-06-07 19:03:23 +0900

    add: スタイル修正

--- components/Header.vue
+++ components/Header.vue
@@ -8,7 +8,7 @@
         <nav class="nav-area">
             <div id="navbutton">
                 <div class="button" v-for="link in links" :key="link.index">
-                    <a :href='"/"+link'><i class="far fa-check-circle"></i> {{ link }}</a>
+                    <a class="text" :href='"/"+link'><i class="far fa-check-circle"></i> {{ link }}</a>
                 </div>
             </div>
         </nav>
