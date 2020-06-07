4715f3dd84010e1385198b9651477127d3d23ad1
4tu <rikuto.tsujimoto++4tu@gmail.com>
2020-06-07 17:59:36 +0900

    fix: url直るかな

--- components/Header.vue
+++ components/Header.vue
@@ -8,7 +8,7 @@
         <nav class="nav-area">
             <div id="navbutton">
                 <div class="button" v-for="link in links" :key="link.index">
-                    <a class="text" :href="path + link"><i class="far fa-check-circle"></i> {{ link }}</a>
+                    <nuxt-link :to='"/"+link'><i class="far fa-check-circle"></i> {{ link }}</nuxt-link>
                 </div>
             </div>
         </nav>
@@ -23,9 +23,6 @@ export default {
             path: '',
             links: ['About', 'Stack', 'Contact'],
         }
-    },
-    created() {
-        this.path = location.protocol + '//' + location.hostname + ':' + location.port + '/'
     }
 }
 </script>
